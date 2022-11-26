bed6706426393b42552f45542e9692d8b595f50a
Worstdudeofalltime <Krisbertolino@gmail.com>
2022-11-26 11:22:49 -0500

    began conversion to the side box vs marker popups

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -6,12 +6,16 @@ import Map, { Marker, Popup } from 'react-map-gl';
 import fridge from './../IMG_0366.jpg';
 import instagram from './../icons8-instagram-50.png';
 import website from './../icons8-website-48.png';
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import mapboxgl from 'mapbox-gl';
+
 const MAPBOX_TOKEN =
   'pk.eyJ1Ijoid29yc3RkdWRlb2ZhbGwiLCJhIjoiY2w1ZzVydndyMWk2MDNiczBnNTNjeHJncSJ9.8c2Pkbc8f5WLoJFjgLBq2A';
 const data = require('./../fridges.json');
 const MapBox = () => {
   const [status, setStatus] = useState(false);
-  const [currentFridge, setCurrentFridge] = useState('');
   const [selectedFridge, setSelectedFridge] = useState(null);
   const [viewState, setViewState] = useState({
     latitude: 39.952,
@@ -20,9 +24,39 @@ const MapBox = () => {
   });
   return (
     <div className="mapHolder">
-      <div className="overlay-box">
-        <h1>PHILADELPHIA COMMUNITY FRIDGES</h1>
-        {status ? <h2>{currentFridge}</h2> : <h2>No Fridge Selected</h2>}
+      <div className="overlay-box ">
+        {status ? (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              textAlign: 'center',
+            }}
+          >
+            <h2>{selectedFridge.properties.description}</h2>
+            <h2>{selectedFridge.properties.address}</h2>
+            <div className="socialBox">
+              <a href={selectedFridge.properties.instagram}>
+                <img
+                  src={instagram}
+                  style={{ height: '30px', width: '30px' }}
+                ></img>
+              </a>
+              <a href={selectedFridge.properties.website} target="_blank">
+                <img
+                  src={website}
+                  style={{ height: '30px', width: '30px' }}
+                ></img>
+              </a>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h1>No fridge has been selected</h1>
+          </>
+        )}
       </div>
       <Map
         {...viewState}
@@ -30,6 +64,9 @@ const MapBox = () => {
         onMove={(evt) => setViewState(evt.viewState)}
         mapboxAccessToken={MAPBOX_TOKEN}
         mapStyle="mapbox://styles/mapbox/dark-v9"
+        onClick={() => {
+          setStatus(null);
+        }}
       >
         {data.features.map((fridge, index) => {
           return (
@@ -44,10 +81,10 @@ const MapBox = () => {
                 alignItems: 'center',
                 flexDirection: 'column',
               }}
-              color="#77cbb9"
               onClick={(e) => {
                 e.originalEvent.stopPropagation();
                 setSelectedFridge(fridge);
+                setStatus(true);
                 setViewState({
                   ...viewState,
                   latitude: fridge.geometry.coordinates[1],
@@ -63,8 +100,11 @@ const MapBox = () => {
             longitude={Number(selectedFridge.geometry.coordinates[0])}
             latitude={Number(selectedFridge.geometry.coordinates[1])}
             anchor="bottom"
-            style={{maxWidth: '430px'}}
-            onClose={() => setSelectedFridge(null)}
+            style={{ maxWidth: '430px' }}
+            onClose={() => {
+              setSelectedFridge(null);
+              setStatus(null);
+            }}
           >
             <div className="pop-up-box">
               <div className="popUpHalf">
@@ -80,21 +120,6 @@ const MapBox = () => {
                   {selectedFridge.properties.title}
                 </h3>
                 <p>{selectedFridge.properties.description}</p>
-
-                <div className="socialBox">
-                  <a href={selectedFridge.properties.instagram}>
-                    <img
-                      src={instagram}
-                      style={{ height: '30px', width: '30px' }}
-                    ></img>
-                  </a>
-                  <a href={selectedFridge.properties.website}>
-                    <img
-                      src={website}
-                      style={{ height: '30px', width: '30px' }}
-                    ></img>
-                  </a>
-                </div>
               </div>
             </div>
           </Popup>
